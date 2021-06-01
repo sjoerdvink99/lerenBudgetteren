6707db718cbb9a877b46485077697083500ec7f3
Sjoerd Vink <sjoerdvink@icloud.com>
2021-06-01 10:17:46 +0200

    optimization

--- src/components/application/DashboardOverview.js
+++ src/components/application/DashboardOverview.js
@@ -1,47 +1,63 @@
-import React from 'react'
+import React from "react";
 import { CSVLink } from "react-csv";
 import { Typography, Button } from "@material-ui/core";
 import IncomeExpenseRow from "./IncomeExpenseRow";
-import './DashboardOverview.css'
+import "./DashboardOverview.css";
 
-export default function DashboardOverview({income, expenses}) {
-    const headers = [
-        { label: "ID", key: "id" },
-        { label: "Timestamp", key: "timestamp" },
-        { label: "Title", key: "title" },
-        { label: "Amount", key: "amount" },
-      ];
-    
-      const csvReport = {
-        filename: 'incomeAndExpenses.csv',
-        headers: headers,
-        data: income,
-      };
+export default function DashboardOverview({ income, expenses }) {
+  const headers = [
+    { label: "ID", key: "id" },
+    { label: "Timestamp", key: "timestamp" },
+    { label: "Title", key: "title" },
+    { label: "Amount", key: "amount" },
+  ];
 
-    return (
+  const csvReport = {
+    filename: "incomeAndExpenses.csv",
+    headers: headers,
+    data: income,
+  };
+
+  return (
     <div className='dashboardOverview'>
-        <div className='dashboardOverview__buttons'>
-            <Typography variant='h4'>Income and expenses</Typography>
-            <Button color='primary' variant='contained'>Add</Button>
+      <div className='dashboardOverview__buttons'>
+        <Typography variant='h4'>Income and expenses</Typography>
+        <Button color='primary' variant='contained'>
+          Add
+        </Button>
+      </div>
+      <div className='dashboardOverview__overview'>
+        <div className='dashboardOverview__income'>
+          {income.map(({ id, title, timestamp, amount }) => (
+            <IncomeExpenseRow
+              key={id}
+              title={title}
+              timestamp={timestamp}
+              amount={amount}
+            />
+          ))}
         </div>
-        <div className='dashboardOverview__overview'>
-            <div className='dashboardOverview__income'>
-            {income.map(({id, title, timestamp, amount}) => (
-                <IncomeExpenseRow key={id} title={title} timestamp={timestamp} amount={amount} />
-            ))}
-            </div>
-            <div className='dashboardOverview__expense'>
-            {expenses.map(({id, title, timestamp, amount}) => (
-                <IncomeExpenseRow key={id} title={title} timestamp={timestamp} amount={amount} />
-            ))}
-            </div>
+        <div className='dashboardOverview__expense'>
+          {expenses.map(({ id, title, timestamp, amount }) => (
+            <IncomeExpenseRow
+              key={id}
+              title={title}
+              timestamp={timestamp}
+              amount={amount}
+            />
+          ))}
         </div>
-        <div className='dashboardOverview__export'>
-            <Button color='primary' variant='contained'>Predictions</Button>
-            <CSVLink {...csvReport}>
-                <Button color='primary' variant='outlined'>Export to Excel</Button>
-            </CSVLink>
-        </div>
+      </div>
+      <div className='dashboardOverview__export'>
+        <Button color='primary' variant='contained'>
+          Predictions
+        </Button>
+        <CSVLink {...csvReport}>
+          <Button color='primary' variant='outlined'>
+            Export to Excel
+          </Button>
+        </CSVLink>
+      </div>
     </div>
-    )
+  );
 }

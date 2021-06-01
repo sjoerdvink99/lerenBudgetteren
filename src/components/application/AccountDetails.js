fafe6f607e1d47e013a317f1a195fcd84aab89f2
Sjoerd Vink <sjoerdvink@icloud.com>
2021-06-01 08:25:17 +0200

    account details

--- src/components/application/AccountDetails.js
+++ src/components/application/AccountDetails.js
@@ -1,13 +1,54 @@
-import React from "react";
-import ProfileSidebar from './ProfileSidebar'
+import React, { useState } from "react";
+import ProfileSidebar from "./ProfileSidebar";
 import "./AccountDetails.css";
+import { Button, TextField, Avatar } from "@material-ui/core";
 
 export default function AccountDetails() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const updateProfile = () => {
+    //
+  };
+
   return (
     <div className='accountDetails'>
       <ProfileSidebar />
       <div className='accountDetails__container'>
-        <h1>Account details</h1>
+        <div className='accountDetails__containerLeft'>
+          <TextField
+            variant='outlined'
+            label='First name'
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <TextField
+            variant='outlined'
+            label='Last name'
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <TextField
+            variant='outlined'
+            label='Phone'
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <TextField
+            variant='outlined'
+            label='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Button color='primary' variant='contained' onClick={updateProfile}>
+            Update profile
+          </Button>
+        </div>
+        <div className='accountDetails__containerRight'>
+          <Avatar src='#' alt='' />
+        </div>
       </div>
     </div>
   );
--- src/components/application/IncomeExpenseRow.js
+++ src/components/application/IncomeExpenseRow.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
-import './IncomeExpenseRow.css'
+import React from "react";
+import { Typography } from "@material-ui/core";
+import salary from "../../assets/salary.png";
+import supermarket from "../../assets/supermarket.png";
+import other from "../../assets/other.png";
+import subscription from "../../assets/subscription.png";
+import "./IncomeExpenseRow.css";
 
-export default function IncomeExpenseRow({title, timestamp, amount}) {
-    return (
-        <div className='incomeExpenseRow'>
-            <div className='incomeExpenseRow__image'>
-                <img src='#' alt={title} />
-            </div>
-            <div className='incomeExpenseRow__text'>
-                <Typography variant='h6'>{title}</Typography>
-                <Typography variant='body1'>{new Date(timestamp?.toDate()).toLocaleDateString("eu-NL", {
+export default function IncomeExpenseRow({ title, timestamp, amount }) {
+  return (
+    <div className='incomeExpenseRow'>
+      <div className='incomeExpenseRow__image'>
+        <img
+          src={
+            title === "Supermarket"
+              ? supermarket
+              : title === "Salary"
+              ? salary
+              : title === "Subscription"
+              ? subscription
+              : other
+          }
+          alt={title}
+        />
+      </div>
+      <div className='incomeExpenseRow__text'>
+        <Typography variant='h6'>{title}</Typography>
+        <Typography variant='body1'>
+          {new Date(timestamp?.toDate()).toLocaleDateString("eu-NL", {
             day: "2-digit",
             month: "2-digit",
             year: "numeric",
-          })}</Typography>
-            </div>
-            <div className='incomeExpenseRow__money'>
-                <Typography variant='body1'>€{amount}</Typography>
-            </div>
-        </div>
-    )
+          })}
+        </Typography>
+      </div>
+      <div className='incomeExpenseRow__money'>
+        <Typography variant='body1'>€{amount}</Typography>
+      </div>
+    </div>
+  );
 }

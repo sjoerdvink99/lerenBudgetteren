48d9a1f9106b37478a1ac079c7dafedb8c76d28d
Sjoerd Vink <sjoerdvink@icloud.com>
2021-06-01 09:33:48 +0200

    chart

--- src/components/application/Chart.js
+++ src/components/application/Chart.js
@@ -1,48 +1,59 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import "./Chart.css";
 
 const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "DD/MM/YY",
+          tooltipFormat: "ll",
+        },
+        display: false,
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+      },
+    ],
+  },
+  parsing: {
+    xAxisKey: "timestamp",
+    yAxisKey: "amount",
+  },
+  plugins: {
     legend: {
       display: false,
     },
-    elements: {
-      point: {
-        radius: 0,
-      },
-    },
-    maintainAspectRatio: false,
-    tooltips: {
-      mode: "index",
-      intersect: false,
-    },
-    scales: {
-      xAxes: [
-        {
-          type: "time",
-          time: {
-            format: "DD/MM/YY",
-            tooltipFormat: "ll",
-          },
-          display: false,
-        },
-      ],
-      yAxes: [
-        {
-          gridLines: {
-            display: false,
-          },
-        },
-      ],
-    },
-  };
+  },
+};
 
-export default function Chart({income, expenses}) {
-  const [data, setData] = useState([])
+export default function Chart({ income, expenses }) {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    setData()
-  }, [])
+    const total = income.concat(expenses);
+    total.forEach((sum) => delete sum.id & delete sum.title);
+    setData(total);
+  }, [expenses, income]);
 
   return (
     <div className='chart'>

4a0951a5de630d338ab314abf67ec4d8545c8926
Sjoerd Vink <sjoerdvink@icloud.com>
2021-06-07 09:36:06 +0200

    profile billing page

--- src/components/application/Billing.js
+++ src/components/application/Billing.js
@@ -1,14 +1,63 @@
-import React from 'react';
-import ProfileSidebar from './ProfileSidebar';
-import './Billing.css'
+import React from "react";
+import {
+  TableContainer,
+  Typography,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+} from "@material-ui/core";
+import ProfileSidebar from "./ProfileSidebar";
+import "./Billing.css";
 
 export default function Billing() {
-    return (
-        <div className='billing'>
-            <ProfileSidebar />
-            <div className='billing__container'>
-                <h1>Billing!!!</h1>
-            </div>
+  return (
+    <div className='billing'>
+      <ProfileSidebar />
+      <div className='billing__container'>
+        <div className='billing__head'>
+          <Typography variant='h4' align='center'>
+            Billing
+          </Typography>
         </div>
-    )
+        <div className='billing__main'>
+          <div className='billing__left'>
+            <Typography variant='h6'>Invoices</Typography>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Amount</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>May 2021</TableCell>
+                    <TableCell>€9,99</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>June 2021</TableCell>
+                    <TableCell>€9,99</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>July 2021</TableCell>
+                    <TableCell>€9,99</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+          <div className='billing__right'>
+            <Typography variant='h6'>Payment method</Typography>
+            <Button variant='contained' color='primary'>
+              Update method
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

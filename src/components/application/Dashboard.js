d24bdd83d8c618b2a36e25f2bd88ad5cb37a564a
Sjoerd Vink <sjoerdvink@icloud.com>
2021-06-14 08:51:22 +0200

    parse html for education posts

--- src/components/application/Dashboard.js
+++ src/components/application/Dashboard.js
@@ -1,43 +1,45 @@
-import React, {useState, useEffect} from "react";
-import {useStateValue} from '../../StateProvider'
-import Chart from './Chart'
-import {db} from '../../firebase'
+import React, { useState, useEffect } from "react";
+import { useStateValue } from "../../StateProvider";
+import Chart from "./Chart";
+import { db } from "../../firebase";
 import DashboardOverview from "./DashboardOverview";
 import ArticleRecommendation from "./ArticleRecommendation";
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  const [{user}] = useStateValue()
-    const [income, setIncome] = useState([])
-    const [expenses, setExpenses] = useState([])
+  const [{ user }] = useStateValue();
+  const [income, setIncome] = useState([]);
+  const [expenses, setExpenses] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
     db.collection(`users/${user.uid}/income`)
       .limit(3)
-      .onSnapshot((snapshot) => 
-        setIncome(snapshot.docs.map((doc) => ({
-          id: doc.id,
-          title: doc.data().title,
-          timestamp: doc.data().timestamp,
-          amount: doc.data().amount,
-        }))
+      .onSnapshot((snapshot) =>
+        setIncome(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            title: doc.data().title,
+            timestamp: doc.data().timestamp,
+            amount: doc.data().amount,
+          }))
         )
-      ) 
-    }, [user.uid])
+      );
+  }, [user.uid]);
 
-    useEffect(() => {
+  useEffect(() => {
     db.collection(`users/${user.uid}/expenses`)
       .limit(3)
-      .onSnapshot((snapshot) => 
-        setExpenses(snapshot.docs.map((doc) => ({
-          id: doc.id,
-          title: doc.data().title,
-          timestamp: doc.data().timestamp,
-          amount: doc.data().amount,
-        }))
+      .onSnapshot((snapshot) =>
+        setExpenses(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            title: doc.data().title,
+            timestamp: doc.data().timestamp,
+            amount: doc.data().amount,
+          }))
         )
-      ) 
-    }, [user.uid])
+      );
+  }, [user.uid]);
 
   return (
     <div className='dashboard'>
--- src/components/application/Post.js
+++ src/components/application/Post.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 import { db } from "../../firebase";
+import ReactHtmlParser from "react-html-parser";
 import "./Post.css";
 
 export default function Post() {
@@ -9,21 +10,26 @@ export default function Post() {
   const [post, setPost] = useState({});
 
   useEffect(() => {
-    db.doc(`articles/${postId}`).get().then((post) =>
-      setPost({
-        title: post.data().title,
-        imageUrl: post.data().imageUrl,
-        timestamp: post.data().timestamp,
-      })
-    );
+    db.doc(`articles/${postId}`)
+      .get()
+      .then((post) =>
+        setPost({
+          title: post.data().title,
+          imageUrl: post.data().imageUrl,
+          timestamp: post.data().timestamp,
+          text: post.data().text,
+        })
+      );
   }, [postId]);
 
   return (
     <div className='post'>
       <div className='post__container'>
         <img src={post.imageUrl} alt={post.title} />
-        <Typography variant='h4' align='center'>{post.title}</Typography>
-        <Typography variant='body1'>{post.text}</Typography>
+        <Typography variant='h4' align='center'>
+          {post.title}
+        </Typography>
+        <Typography variant='body1'>{ReactHtmlParser(post.text)}</Typography>
       </div>
     </div>
   );
